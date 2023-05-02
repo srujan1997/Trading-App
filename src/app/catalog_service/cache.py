d5cf86a7623c2bd773c93f72a43aada986acb518
srujan <srujan1997@gmail.com>
2023-05-01 21:55:08 -0400

    bug fixes related to caching in catalog service

--- src/app/catalog_service/cache.py
+++ src/app/catalog_service/cache.py
@@ -1,8 +1,15 @@
-from flask import current_app as app
+import redis
 
 
 def delete_from_redis(key):
-    redis_conn = app.redis_connection
+    redis_conn = redis.StrictRedis(
+        host="redis",
+        port=6379,
+        db=0,
+        password="",
+        socket_timeout=2,
+        socket_connect_timeout=2,
+    )
     if redis_conn:
         redis_conn.delete(key)
     return None
